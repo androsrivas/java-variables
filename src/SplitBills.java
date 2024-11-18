7e7ac3f1d6e28ac93223b41a32c026c541b8fa09
dustisme <androsrivas@outlook.com>
2024-11-18 13:45:13 +0100

    finish third exercise

--- src/SplitBills.java
+++ src/SplitBills.java
@@ -1,11 +1,15 @@
 public class SplitBills {
     public static void main(String[] args) {
 
-        //Crea dos variables, la primera que contenga el total de la cuenta que será 7 EUR y la segunda que contenga el número de personas a dividir (esta variable deberá ser de tipo int) que serán 2 personas.
-
-        //Haz la operación aritmética para devolver la cantidad a pagar por persona y almacénala dentro de una variable (del tipo que veas conveniente).
-        
+        //Crea dos variables, la primera que contenga el total de la cuenta que será 7 EUR
+        // y la segunda que contenga el número de personas a dividir (esta variable deberá ser de tipo int)
+        // que serán 2 personas.
+        int total = 7;
+        int totalPeople = 2;
+        //Haz la operación aritmética para devolver la cantidad a pagar por persona
+        // y almacénala dentro de una variable (del tipo que veas conveniente).
+        double payPerPerson = (double) total / totalPeople;
         //Imprime el resultado (deberá devolverte 3.5)
-
+        System.out.println(payPerPerson);
     }
 }
