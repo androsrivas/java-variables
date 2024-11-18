d79c45737cae51cadb5fc55f77223d4777459b66
dustisme <androsrivas@outlook.com>
2024-11-18 13:32:48 +0100

    first exercise done

--- src/Welcome.java
+++ src/Welcome.java
@@ -1,5 +1,5 @@
-//Crea la clase Welcome dentro de este archivo
-//Crea el método main
-//Imprime en consola "Welcome to Java World!"
-//Compila y ejecuta el archivo
-//El resultado correcto es poder ver en la terminal la frase: Welcome to Java World!
+public class Welcome {
+    public static void main (String[] args) {
+        System.out.println("Welcome to the Java world!");
+    }
+}
